ea976aae45aa1e2b85ac9e4ed39d2a16ebbe4556
Hrishikesh332 <93283437+Hrishikesh332@users.noreply.github.com>
2022-10-18 20:38:28 +0530

    Add files via upload

--- Adopet.py
+++ Adopet.py
@@ -1,17 +1,13 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import os
-import plotly.express as px
-from plotly import graph_objects as go
-from plotly.subplots import make_subplots
 from streamlit_folium import folium_static
 import folium
 from streamlit_option_menu import option_menu
 import streamlit.components.v1 as components
-import matplotlib.pyplot as plt
-import seaborn as sns
 
+
+df=pd.read_csv('petd.csv')
 try:
     import folium as f
 except Exception:
@@ -27,4 +23,19 @@ selected = option_menu(
         )
 if (selected=="About Us"):
     st.write("Adopet")
+if (selected=="Pet"):
     
+    animal=st.selectbox("Animal Type: ", df.Animal.unique().tolist())
+    breed=st.selectbox("Breed Type: ", df.Type.unique().tolist())
+    age1= st.selectbox("Age: ", df.Age.unique().tolist())
+    gender=st.selectbox("Gender: ", df.Gender.unique().tolist())
+    size=st.selectbox("Size: ", df.Size.unique().tolist())
+    fur_type=st.selectbox("Fur: ", df.Fur.unique().tolist())
+    color1=st.selectbox("Color of Fur: ", df.Color.unique().tolist())
+    trained=st.selectbox("Training Status: ", df.Trained.unique().tolist())
+    vaccinated=st.multiselect("Vaccination Status: ", df.Vaccinated.unique().tolist())
+    picture = st.camera_input("Take a picture")
+    lang=st.text_input("Longitude", "")
+    lati=st.text_input("Langitude", "")
+    
+
